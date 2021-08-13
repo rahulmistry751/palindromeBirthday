f189f6f748aa046a53cfbbeff75664d67fbc1aad
rahulmistry751 <rahulmistry751@gmail.com>
2021-08-13 18:29:05 +0530

    feat: show next palindrome birthdate

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const checkBtn=document.getElementById('check-btn');
 checkBtn.addEventListener("click",()=>{
     if(dateInput.value){
         show.innerHTML='<img src="del.svg" alt="">';
+        // callBack will trigger after 2sec
         setTimeout(checkPalindrome, 2000);
         
     }
     else{
+        //If input value is empty
         show.textContent="Please enter date";
     }
     
@@ -18,59 +20,144 @@ checkBtn.addEventListener("click",()=>{
 })
 // Functions
 function checkPalindrome(){
-    let date=[];let flag='';
-    // remove - from the date format
-    date=dateInput.value.split("-");
-    //save month, year , day into seperate variables using index
-    let year=date[0];
-    let month=date[1];
-    let day=date[2];
-    let yyyy_mm_dd=year+month+day;
-    let dd_mm_yyyy=day+month+year;
-    let mm_dd_yy=month+day+year.substring(2);
-    let m_dd_yyyy=Number(month)+day+year;
-    //console.log(yyyy_mm_dd,dd_mm_yyyy,mm_dd_yy,m_dd_yyyy);
-    // Check every formats palindrome
-    switch (true){
-        case isPalindrome(yyyy_mm_dd):      
-            flag=`${year}-${month}-${day}`;
-            //console.log(flag);
-            break;
-        case isPalindrome(dd_mm_yyyy):
-            flag=`${day}-${month}-${year}`;
-            //console.log(flag);
-            break;
-        case isPalindrome(mm_dd_yy):
-            flag=`${month}-${day}-${year}`;
-            //console.log(flag);
-            break;
-        case isPalindrome(m_dd_yyyy):
-            flag=`${Number(month)}-${day}-${year}`;
-            //console.log(flag);
-            break;
+    // Split the input year-month-day into array
+    let date=dateInput.value.split("-");
+    console.log(date);
+    // Save date in object
+    let dateObj={
+        "year":date[0],
+        "month":date[1],
+        "day":date[2],
+    };
+    let result=createDateFormats(dateObj);
+    if(result[1]){      // Checks if birthday is palindrome or not
+        show.textContent=`Yay! Your Birthday is a Palindrome in ${result[1]} format.`
     }
-    //console.log(flag)
-    checkFlag(flag);
+    else{
+        checkNextPalindrome(result[0]);  // Looks for next palindrome birthdate
+    }
+
 }
-function isPalindrome(str){
-    
-    for(let i=0;i<Math.floor(str.length/2);i++){
-        // Checks if 1-last, 2-secondlast and so on index values are equal are not.
-        if(str[i]!=str[str.length-(i+1)]){
-        //console.log(str[i],str[str.length-(i+1)])
+
+function createDateFormats(date){
+        // Creating different date formats
+        //dd-mm-yyyy, mm-dd-yyyy, dd-mm-yy, mm-dd-yy
+        let flag='';
+        let year=date.year;
+        let month=date.month;
+        let day=date.day;
+        let dd_mm_yyyy=day+month+year;
+        let mm_dd_yyyy=month+day+year;
+        let dd_mm_yy=day+month+(year.substring(2));
+        let mm_dd_yy=month+day+year.substring(2);
+
+        switch (true){
+            case isPalindrome(dd_mm_yyyy):      
+                flag=`${day}-${month}-${year}`;
+                
+                break;
+            case isPalindrome(mm_dd_yyyy):
+                flag=`${month}-${day}-${year}`;
+                
+                break;
+            case isPalindrome(mm_dd_yy):
+                flag=`${month}-${day}-${year.substring(2)}`;
+                
+                break;
+            case isPalindrome(dd_mm_yy):
+                flag=`${day}-${month}-${year.substring(2)}`;
+                
+                break;
+        }
+        return ([date,flag]);
+    }
+
+function isPalindrome(date){
+    for(let i=0;i<Math.floor(date.length/2);i++){
+        // Checks if 1-last, 2-secondlast and so on, index values are equal are not.
+        if(date[i]!=date[date.length-(i+1)]){
             return false;
         }
-        
+            
     }
     return true;
 }
-// Checks if any format raised a flag
-function checkFlag(flag){
-    if (flag){
-        show.innerText=`Yaay!!! Your birth-date in format ${flag} is a palindrome`;
+
+
+function nextDate(date){
+    let year=date.year;
+    let month=date.month;
+    let day=date.day;
+    let newDay=Number(day)+1;
+    let newMonth=Number(month);
+    let newYear=Number(year);
+    let monthDays=[30,28,31,30,31,30,31,31,30,31,30,31]; // Days in each month
+
+    if(newMonth==2){    //Checks for leap year
+        if(checkLeapYear(newYear)){
+            if(newDay>29){
+                newDay=1;
+                newMonth++;
+            }
+            
+        }
+        else if(newDay>28){
+            newDay=1;
+            newMonth++;
+        }
     }
-    else{
-        show.innerText="Your birthdate is not a palindrome!";
+    else{  
+        //Checks if incremented day is greater than month's total days
+        if(newDay>monthDays[newMonth-1]){   
+            newDay=1;
+            newMonth++;
+
+        }
+
+        else if(newMonth>12){
+            newMonth=1;
+            newDay=1;
+            newYear++;
+
+        }
     }
 
+    if(newMonth<10){ 
+        newMonth="0"+newMonth.toString();
+    }
+    if(newDay<10){
+        newDay="0"+newDay.toString();
+    }
+
+    return{
+        "year":newYear.toString(),
+        "month":newMonth.toString(),
+        "day":newDay.toString(),
+    };
+}
+function checkLeapYear(year){
+    if(year%400==0 && year%100!=0 || year%4==0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+function checkNextPalindrome(date){
+    let result;let counter=1;
+    let newDate=nextDate(date);
+    while(true){
+        
+        result= createDateFormats(newDate);
+        if(!result[1]){
+            newDate=nextDate(result[0]);
+            counter++;
+        }
+        else{
+            show.textContent=`The next palindrome date is on ${result[1]}, you missed it by ${counter} days! 😔`
+            break;
+        }
+
+    }
+    
 }
